b055b3ae024e3db9dcae8401d9587c5d2c4dfecf
pathompat <pathompat.s@gmail.com>
2022-12-30 14:23:17 +0700

    enable auth guard

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -22,7 +22,7 @@ export class ProductsController {
     return this.productsService.create(createProductDto)
   }
 
-  // @UseGuards(AuthGuard()) // Error cannot auth
+  @UseGuards(AuthGuard())
   @Get()
   findAll() {
     return this.productsService.findAll()
