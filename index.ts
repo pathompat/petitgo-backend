6bd6c3435755d9d792a2dbe2e1eb565ff1a0840b
pathompat <lw93430@linecorp.com>
2024-05-22 00:54:42 +0700

    update firebase cloud function to v2

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { ExpressAdapter } from '@nestjs/platform-express'
 import * as express from 'express'
-import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
 import { AppModule } from './src/app.module'
 import { ValidationPipe } from '@nestjs/common'
 
+import { config } from 'firebase-functions'
+import { onRequest } from 'firebase-functions/v2/https'
+import { credential } from 'firebase-admin'
+import { initializeApp } from 'firebase-admin/app'
+
 const expressServer = express()
 
-admin.initializeApp({
-  credential: admin.credential.cert(functions.config().petitgo_backend_config),
+initializeApp({
+  credential: credential.cert(config().petitgo_backend_config),
 })
 
 const createFunction = async (expressInstance): Promise<void> => {
@@ -21,7 +24,8 @@ const createFunction = async (expressInstance): Promise<void> => {
   app.enableCors()
   await app.init()
 }
-export const api = functions.https.onRequest(async (request, response) => {
+
+export const api = onRequest(async (request, response) => {
   await createFunction(expressServer)
   expressServer(request, response)
 })
