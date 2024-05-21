bbb5f9fcc4668ea976402a7cde894695466bca68
pathompat <lw93430@linecorp.com>
2024-05-22 02:38:47 +0700

    remove firebase config import

--- index.ts
+++ index.ts
@@ -4,16 +4,12 @@ import * as express from 'express'
 import { AppModule } from './src/app.module'
 import { ValidationPipe } from '@nestjs/common'
 
-import { config } from 'firebase-functions'
 import { onRequest } from 'firebase-functions/v2/https'
-import { credential } from 'firebase-admin'
 import { initializeApp } from 'firebase-admin/app'
 
 const expressServer = express()
 
-initializeApp({
-  credential: credential.cert(config().petitgo_backend_config),
-})
+initializeApp()
 
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
@@ -25,7 +21,7 @@ const createFunction = async (expressInstance): Promise<void> => {
   await app.init()
 }
 
-export const api = onRequest(async (request, response) => {
+exports.api = onRequest(async (request, response) => {
   await createFunction(expressServer)
   expressServer(request, response)
 })
